1e622c42e470d9ac17ad63119afe74fa7d7dc09e
Tejasvee Dighe <tejasveedighe@gmail.com>
2024-03-21 18:20:24 +0530

    Update Property

--- AdoDemo/Form1.Designer.cs
+++ AdoDemo/Form1.Designer.cs
@@ -32,9 +32,21 @@
             tabControl1 = new TabControl();
             users_tab = new TabPage();
             properties_tab = new TabPage();
-            dataGridView1 = new DataGridView();
-            tabPage3 = new TabPage();
+            update_btn = new Button();
+            type_cb = new ComboBox();
+            status_cb = new ComboBox();
+            contact_txt = new TextBox();
+            squareFeet_txt = new TextBox();
+            bathroom_Txt = new TextBox();
+            bedroom_txt = new TextBox();
+            price_txt = new TextBox();
+            property_location_txt = new TextBox();
+            property_title_txt = new TextBox();
+            refreshBtn = new Button();
+            label2 = new Label();
             properties_add_property_btn = new Button();
+            dataGridView1 = new DataGridView();
+            desc_txt = new TextBox();
             tabControl1.SuspendLayout();
             properties_tab.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
@@ -54,7 +66,6 @@
             // 
             tabControl1.Controls.Add(users_tab);
             tabControl1.Controls.Add(properties_tab);
-            tabControl1.Controls.Add(tabPage3);
             tabControl1.Dock = DockStyle.Fill;
             tabControl1.Location = new Point(0, 0);
             tabControl1.Name = "tabControl1";
@@ -75,6 +86,19 @@
             // 
             // properties_tab
             // 
+            properties_tab.Controls.Add(desc_txt);
+            properties_tab.Controls.Add(update_btn);
+            properties_tab.Controls.Add(type_cb);
+            properties_tab.Controls.Add(status_cb);
+            properties_tab.Controls.Add(contact_txt);
+            properties_tab.Controls.Add(squareFeet_txt);
+            properties_tab.Controls.Add(bathroom_Txt);
+            properties_tab.Controls.Add(bedroom_txt);
+            properties_tab.Controls.Add(price_txt);
+            properties_tab.Controls.Add(property_location_txt);
+            properties_tab.Controls.Add(property_title_txt);
+            properties_tab.Controls.Add(refreshBtn);
+            properties_tab.Controls.Add(label2);
             properties_tab.Controls.Add(properties_add_property_btn);
             properties_tab.Controls.Add(dataGridView1);
             properties_tab.Controls.Add(label1);
@@ -86,25 +110,108 @@
             properties_tab.Text = "Properties";
             properties_tab.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // update_btn
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Dock = DockStyle.Bottom;
-            dataGridView1.Location = new Point(3, 238);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(1083, 374);
-            dataGridView1.TabIndex = 4;
+            update_btn.Location = new Point(254, 176);
+            update_btn.Name = "update_btn";
+            update_btn.Size = new Size(108, 23);
+            update_btn.TabIndex = 16;
+            update_btn.Text = "Update Property";
+            update_btn.UseVisualStyleBackColor = true;
+            update_btn.Click += update_btn_Click;
             // 
-            // tabPage3
+            // type_cb
             // 
-            tabPage3.Location = new Point(4, 24);
-            tabPage3.Name = "tabPage3";
-            tabPage3.Padding = new Padding(3);
-            tabPage3.Size = new Size(1089, 615);
-            tabPage3.TabIndex = 2;
-            tabPage3.Text = "tabPage3";
-            tabPage3.UseVisualStyleBackColor = true;
+            type_cb.FormattingEnabled = true;
+            type_cb.Items.AddRange(new object[] { "Bunglow", "Flat", "Residence", "House" });
+            type_cb.Location = new Point(201, 136);
+            type_cb.Name = "type_cb";
+            type_cb.Size = new Size(161, 23);
+            type_cb.TabIndex = 15;
+            // 
+            // status_cb
+            // 
+            status_cb.FormattingEnabled = true;
+            status_cb.Items.AddRange(new object[] { "Sale", "Rent" });
+            status_cb.Location = new Point(201, 94);
+            status_cb.Name = "status_cb";
+            status_cb.Size = new Size(161, 23);
+            status_cb.TabIndex = 14;
+            // 
+            // contact_txt
+            // 
+            contact_txt.Location = new Point(201, 51);
+            contact_txt.Name = "contact_txt";
+            contact_txt.PlaceholderText = "Contact Number";
+            contact_txt.Size = new Size(161, 23);
+            contact_txt.TabIndex = 13;
+            // 
+            // squareFeet_txt
+            // 
+            squareFeet_txt.Location = new Point(201, 12);
+            squareFeet_txt.Name = "squareFeet_txt";
+            squareFeet_txt.PlaceholderText = "Square Feet";
+            squareFeet_txt.Size = new Size(161, 23);
+            squareFeet_txt.TabIndex = 12;
+            // 
+            // bathroom_Txt
+            // 
+            bathroom_Txt.Location = new Point(18, 176);
+            bathroom_Txt.Name = "bathroom_Txt";
+            bathroom_Txt.PlaceholderText = "No of Bathroom";
+            bathroom_Txt.Size = new Size(161, 23);
+            bathroom_Txt.TabIndex = 11;
+            // 
+            // bedroom_txt
+            // 
+            bedroom_txt.Location = new Point(18, 136);
+            bedroom_txt.Name = "bedroom_txt";
+            bedroom_txt.PlaceholderText = "No of Bedroom";
+            bedroom_txt.Size = new Size(161, 23);
+            bedroom_txt.TabIndex = 10;
+            // 
+            // price_txt
+            // 
+            price_txt.Location = new Point(18, 94);
+            price_txt.Name = "price_txt";
+            price_txt.PlaceholderText = "Price";
+            price_txt.Size = new Size(161, 23);
+            price_txt.TabIndex = 9;
+            // 
+            // property_location_txt
+            // 
+            property_location_txt.Location = new Point(18, 51);
+            property_location_txt.Name = "property_location_txt";
+            property_location_txt.PlaceholderText = "Property Location";
+            property_location_txt.Size = new Size(161, 23);
+            property_location_txt.TabIndex = 8;
+            // 
+            // property_title_txt
+            // 
+            property_title_txt.Location = new Point(18, 12);
+            property_title_txt.Name = "property_title_txt";
+            property_title_txt.PlaceholderText = "Property Title";
+            property_title_txt.Size = new Size(161, 23);
+            property_title_txt.TabIndex = 7;
+            // 
+            // refreshBtn
+            // 
+            refreshBtn.Location = new Point(859, 209);
+            refreshBtn.Name = "refreshBtn";
+            refreshBtn.Size = new Size(108, 23);
+            refreshBtn.TabIndex = 6;
+            refreshBtn.Text = "Refresh";
+            refreshBtn.UseVisualStyleBackColor = true;
+            refreshBtn.Click += refreshBtn_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 217);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 15);
+            label2.TabIndex = 5;
+            label2.Text = "All Properties";
             // 
             // properties_add_property_btn
             // 
@@ -115,6 +222,30 @@
             properties_add_property_btn.Text = "Add Property";
             properties_add_property_btn.UseVisualStyleBackColor = true;
             properties_add_property_btn.Click += properties_add_property_btn_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Bottom;
+            dataGridView1.Location = new Point(3, 238);
+            dataGridView1.MultiSelect = false;
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(1083, 374);
+            dataGridView1.TabIndex = 4;
+            dataGridView1.CellDoubleClick += dataGridView1_CellDoubleClick;
+            // 
+            // desc_txt
+            // 
+            desc_txt.Location = new Point(390, 12);
+            desc_txt.Multiline = true;
+            desc_txt.Name = "desc_txt";
+            desc_txt.PlaceholderText = "Description";
+            desc_txt.Size = new Size(199, 147);
+            desc_txt.TabIndex = 17;
             // 
             // Form1
             // 
@@ -138,7 +269,19 @@
         private TabPage users_tab;
         private TabPage properties_tab;
         private DataGridView dataGridView1;
-        private TabPage tabPage3;
         private Button properties_add_property_btn;
+        private Label label2;
+        private Button refreshBtn;
+        private TextBox price_txt;
+        private TextBox property_location_txt;
+        private TextBox property_title_txt;
+        private ComboBox type_cb;
+        private ComboBox status_cb;
+        private TextBox contact_txt;
+        private TextBox squareFeet_txt;
+        private TextBox bathroom_Txt;
+        private TextBox bedroom_txt;
+        private Button update_btn;
+        private TextBox desc_txt;
     }
 }
--- AdoDemo/Form1.cs
+++ AdoDemo/Form1.cs
@@ -2,11 +2,13 @@ using Microsoft.Data.SqlClient;
 // this class has the configuration manager
 using System.Configuration;
 using System.Data;
+using System.Windows.Forms;
 
 namespace AdoDemo
 {
     public partial class Form1 : Form
     {
+        int PropertyId { get; set; }
         public Form1()
         {
             InitializeComponent();
@@ -63,24 +65,90 @@ namespace AdoDemo
         private void properties_add_property_btn_Click(object sender, EventArgs e)
         {
             new AddPropertyForm().Show();
-            //using (SqlConnection conn = new SqlConnection())
-            //{
-            //    conn.ConnectionString = "Data Source=SQL12-16-LATEST\\SQL2016;Initial Catalog=SNW;User ID=nagesh;Password=Download@1;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
-            //    try
-            //    {
-            //        SqlCommand cmd = new SqlCommand();
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //    }
-            //    finally
-            //    {
-            //        conn.Close();
-            //    }
-            //}
-
-
         }
+
+        private void refreshBtn_Click(object sender, EventArgs e)
+        {
+            this.Form1_Load(sender, e);
+            MessageBox.Show("Refresh Success");
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            if (rowIndex >= 0 && rowIndex < dataGridView1.Rows.Count)
+            {
+                // Get the cells of the double-clicked row
+                DataGridViewRow row = dataGridView1.Rows[rowIndex];
+
+                // Set the values of TextBox and ComboBox controls based on the cell values
+                PropertyId = (int)row.Cells["PropertyId"].Value;
+                property_title_txt.Text = row.Cells["PropertyTitle"].Value.ToString();
+                type_cb.SelectedItem = row.Cells["PropertyType"].Value.ToString();
+                property_location_txt.Text = row.Cells["Location"].Value.ToString();
+                price_txt.Text = row.Cells["Price"].Value.ToString();
+                bedroom_txt.Text = row.Cells["NoBedroom"].Value.ToString();
+                bathroom_Txt.Text = row.Cells["NoBathroom"].Value.ToString();
+                squareFeet_txt.Text = row.Cells["SquareFeet"].Value.ToString();
+                status_cb.SelectedItem = row.Cells["Status"].Value.ToString();
+                contact_txt.Text = row.Cells["ContactNumber"].Value.ToString();
+                desc_txt.Text = row.Cells["Description"].Value.ToString();
+            }
+        }
+
+        private void update_btn_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection("Data Source=SQL12-16-LATEST\\SQL2016;Initial Catalog=SNW;User ID=nagesh;Password=Download@1;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False"))
+            {
+                string query = @"UPDATE lpProperty 
+                     SET PropertyTitle = @PropertyTitle, 
+                         PropertyType = @PropertyType, 
+                         Location = @Location, 
+                         Price = @Price, 
+                         NoBedroom = @NoBedroom, 
+                         NoBathroom = @NoBathroom, 
+                         SquareFeet = @SquareFeet, 
+                         Description = @Description, 
+                         Status = @Status, 
+                         DateUpdated = @DateUpdated,
+                         ContactNumber = @ContactNumber
+                     WHERE PropertyId = @PropertyId";
+
+                SqlCommand command = new SqlCommand(query, conn);
+                // Add parameters to the command
+                command.Parameters.AddWithValue("@PropertyTitle", property_title_txt.Text);
+                command.Parameters.AddWithValue("@PropertyType", type_cb.SelectedItem.ToString());
+                command.Parameters.AddWithValue("@Location", property_location_txt.Text);
+                command.Parameters.AddWithValue("@Price", price_txt.Text);
+                command.Parameters.AddWithValue("@NoBedroom", bedroom_txt.Text);
+                command.Parameters.AddWithValue("@NoBathroom", bathroom_Txt.Text);
+                command.Parameters.AddWithValue("@SquareFeet", squareFeet_txt.Text);
+                command.Parameters.AddWithValue("@Status", status_cb.SelectedItem.ToString());
+                command.Parameters.AddWithValue("@DateUpdated", DateTime.Now);
+                command.Parameters.AddWithValue("@PropertyId", PropertyId);
+                command.Parameters.AddWithValue("@ContactNumber", contact_txt.Text);
+                command.Parameters.AddWithValue("@Description", desc_txt.Text);
+
+                try
+                {
+                    // Open the connection
+                    conn.Open();
+
+                    // Execute the command
+                    int rowsAffected = command.ExecuteNonQuery();
+                    MessageBox.Show(rowsAffected + " row(s) updated.");
+                    refreshBtn_Click(sender, e);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error updating data: " + ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
     }
 }
